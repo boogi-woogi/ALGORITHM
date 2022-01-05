00f5fb6f6bbaa293788abe1cf7b4477ae0bfb025
boogi-woogi <88770426+boogi-woogi@users.noreply.github.com>
2022-01-05 22:41:46 +0900

    01.05
    1292(쉽게 푸는 문제)
    for문에 대해서 틀에 박힌 생각을 가지지 말자!

--- test/1292(손풀기).cpp
+++ test/1292(손풀기).cpp
@@ -1,23 +1,25 @@
-//#include<iostream>
-//using namespace std;
-//int a, b;
-//int arr[1001];
-//int answer = 0;
-//void init() {
-//	int cnt = 1;
-//	for (int i = 1; i <= 1000; i++) {
-//	
-//	}
-//}
-//
-//int main(){
-//	init();
-//	cin >> a >> b;
-//	for (int i = a; i <= b; i++) {
-//		answer += arr[i];
-//	}
-//	cout << answer << endl;
-//	for (int i = 1; i <= 7; i++) {
-//		cout << arr[i] << ' ';
-//	}
-//}
+#include<iostream>
+using namespace std;
+int a, b;
+int arr[1001];
+
+int answer = 0;
+void init() {
+	int cnt = 1;
+	for (int i = 1; i < 1001;) {
+		for (int j = 0; j < cnt; j++) {
+			arr[i++] = cnt;
+		}
+		cnt++;
+	}
+
+}
+
+int main(){
+	init();
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		answer += arr[i];
+	}
+	cout << answer << endl;
+}
